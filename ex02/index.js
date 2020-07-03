632ac0ed16e9bc3001c46cc28bbca017db4ccd06
itw_huyue02 <itw_huyue02@tkmail.com>
2020-07-03 18:13:48 +0800

    node第二次作业进行中

--- ex02/index.js
+++ ex02/index.js
@@ -1,18 +1,37 @@
-module.exports.compose = middlewares => {
-    return function () {
-        return dispatch(0)
-        function dispatch(i) {
-            // ##BEGIN## 代码已加密
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEJOSSOEAJEHOSEOESJEHJXIJEHOEOOSXOSOOSOOEJOSSOPSOOIOEXOSSOEIOOOOSXOOE
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOSXOSEJEHJPPJEXOSEOESJPHJEHOPX
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOEAOPJOEXOESJEHJAAOEXOEEOEOOSXOEIOSSJHOOEXOSSOEIOEEOEJOPOOSSJPPJPH
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOPA
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOEAOPJOEXOESJEHJAAOEXOEEOEOOSXOEIOSSJHOOEXOSSOEIOEEOEJOPOOSSJPP
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOSEOESJPPOSEOPJOESOSJOEAOSXOEEOESJEHOESOSSOPEOEAJPPJPHJEHOPX
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOEAOPJOEXOESJEHOSOOSXOEIOEPOOIOEAOSJOSHJPPOSXJEHJPIJEHJHPJPH
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOPAJPH
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJPH
-            // ##END##
-        }
+// module.exports.compose = middlewares => {
+//     return function () {
+//         return dispatch(0)
+//         function dispatch(i) {
+//             // ##BEGIN## 代码已加密
+//             let fn = middlewares[i]
+//             if (!fn) {
+//               return Promise.resolve()
+//             }
+//             return Promise.resolve(
+//               fn(function next () {
+//                 return dispatch(i + 1)
+//               })
+//             )
+//             // ##END##
+//         }
+//     }
+// }
+function next (fn) {
+  return () => {
+    if (!fn) {
+      return Promise.resolve()
     }
+    return Promise.resolve(fn(next))
+  }
+}
+module.exports.compose = middlewares => {
+  if (middlewares.length === 0) return
+  if (middlewares.length === 1) return middlewares[0]
+  return middlewares.reduce((a, b) => {
+    return () => {
+      return Promise.resolve(
+        a(next(b))
+      )
+    }
+  })
 }
